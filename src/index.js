861355489c469b5f55a64483e1b92bfd332ddeac
github username <github email address>
2021-12-21 10:47:41 -0600

    Starter Code

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function addContentToDom(info) {
   
   info.forEach(book => {
     const li = document.createElement('li')
-    li.addEventListener("click", displayBookInfo)
+    li.addEventListener("click", actionsToDisplayBookInfo)
     li.innerText = book.title
     li.id = book.id
     list.append(li)
@@ -27,22 +27,70 @@ function hideElement(el) {
   el.style.display = 'none' 
 }
 
-function displayBookInfo(event) {
+function actionsToDisplayBookInfo(event) {
   li = event.target
   hideElement(li.parentNode)
   
   const foundBook = arr.find(book => book.id === parseInt(li.id))
 
+  displayBookInfo(foundBook)
+}
+
+function displayBookInfo(book) {
   const bookDiv = document.querySelector('.bookInfo')
 
   const h3Tag = document.createElement('h3')
-  h3Tag.innerText = foundBook.title
+  h3Tag.innerText = book.title
 
   const pTag = document.createElement('p')
-  pTag.innerText = foundBook.summary
+  pTag.innerText = book.summary
  
   bookDiv.append(h3Tag, pTag)
 }
+
+function handleAddBookBtnClick(event) {
+  // alert('Btn clicked')
+
+}
+
+function  dynamicallyAddFormImageInput(){
+  // 1) create label
+  // 2) set attractibute
+  // 3) get parent element
+  // 4) attend to DOM
+
+
+}
+
+
+function dynamicallyAddSubmitBtn(){
+
+}
+
+function handleForm(event) {
+  // 1) check connection
+  // 2) prevent default actions
+  // 3) grab use input
+  // 4) display new info
+  // - remove / hide other content from the DOM
+  // - helpful reference: https://www.javascripttutorial.net/javascript-dom/javascript-form/
+
+  // BONUS
+  // 5) save / store input for later use 
+  // 6) clear & hide form
+  // 7) add back / view list
+
+
+ 
+
+}
+
+
+// BONUS
+// code out Display List button functionality
+// Update list
+
+
 
 // debugger
 window.addEventListener('DOMContentLoaded', getData)
